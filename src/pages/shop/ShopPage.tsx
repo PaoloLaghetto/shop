c3a09b6b39d0773f9250b535f4a68cc12bdfe5f1
Paolo Laghetto <paolo.laghetto@gmail.com>
2024-08-09 11:18:33 +0200

    create ProductCard component

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,10 +1,12 @@
 import { Product } from "../../model/product.ts";
 import { useEffect, useState } from "react";
 import { pb } from "../../pocketbase.ts";
+import { ProductCard } from "./components/ProductCard.tsx";
 
 export function ShopPage() {
   // hook useState per aggiornare componente ogni volta che lo stato cambia
   const [products, setProducts] = useState<Product[]>([]);
+  const [pending, setPending] = useState<boolean>(true);
 
   // hook useEffect con [] viene eseguito solo una volta quando viene instanziato il componente
   useEffect(() => {
@@ -16,17 +18,26 @@ export function ShopPage() {
       .getList<Product>()
       .then((res) => {
         setProducts(res.items);
+        setPending(false);
       });
   }
 
-  console.log(products);
+  function addToCart(p: Partial<Product>) {
+    console.log(p);
+  }
+
   return (
     <div>
       <h1 className="title">SHOP</h1>
 
-      {products.map((p) => {
-        return <li key={p.id}>{p.name}</li>;
-      })}
+      {/*visualizzo il loading solo quando pending è true*/}
+      {pending && <div>Loading...</div>}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
+        {products.map((p) => {
+          return <ProductCard key={p.id} product={p} onAddToCart={addToCart} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/pages/shop/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import { Product } from "../../../model/product.ts";
+
+interface ProductCardProps {
+  product: Product;
+  onAddToCart: (product: Product) => void;
+}
+export function ProductCard(props: ProductCardProps) {
+  const { product: p, onAddToCart } = props;
+  return (
+    <div
+      key={p.id}
+      className="bg-white text-black rounded-xl shadow-2xl overflow-hidden"
+    >
+      <img src={p.img} alt={p.name} className="object-cover w-full h-64 " />
+
+      <div className="flex justify-between items-center gap-3 p-3 text-2xl font-bold">
+        <div>{p.name}</div>
+        <div>€ {p.cost}</div>
+      </div>
+
+      <p className="p-3">{p.description}</p>
+
+      <button
+        onClick={() => onAddToCart(p)}
+        className="text-white bg-sky-600 hover:bg-slate-400 transition w-full text-center font-bold p-3 "
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+}
