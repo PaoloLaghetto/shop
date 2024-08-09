2997576b1c4162f626e2cdf1ba0e76355916f2ef
Paolo Laghetto <paolo.laghetto@gmail.com>
2024-08-09 10:38:27 +0200

    add environment variables and get data from pocketbase

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,20 +1,32 @@
-import PocketBase from "pocketbase";
-export const pb = new PocketBase("http://127.0.0.1:8090");
+import { Product } from "../../model/product.ts";
+import { useEffect, useState } from "react";
+import { pb } from "../../pocketbase.ts";
 
 export function ShopPage() {
+  // hook useState per aggiornare componente ogni volta che lo stato cambia
+  const [products, setProducts] = useState<Product[]>([]);
+
+  // hook useEffect con [] viene eseguito solo una volta quando viene instanziato il componente
+  useEffect(() => {
+    loadData();
+  }, []);
+
   function loadData() {
     pb.collection("products")
-      .getList()
-      .then((data) => {
-        console.log(data);
+      .getList<Product>()
+      .then((res) => {
+        setProducts(res.items);
       });
   }
+
+  console.log(products);
   return (
     <div>
       <h1 className="title">SHOP</h1>
-      <button className="btn" onClick={loadData}>
-        load data
-      </button>
+
+      {products.map((p) => {
+        return <li key={p.id}>{p.name}</li>;
+      })}
     </div>
   );
 }
