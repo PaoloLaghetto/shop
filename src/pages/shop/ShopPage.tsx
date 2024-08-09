31f43597782e7afe42acec9e61408ce6b7fe371b
Paolo Laghetto <paolo.laghetto@gmail.com>
2024-08-09 11:36:41 +0200

    create spinner

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -2,11 +2,13 @@ import { Product } from "../../model/product.ts";
 import { useEffect, useState } from "react";
 import { pb } from "../../pocketbase.ts";
 import { ProductCard } from "./components/ProductCard.tsx";
+import { ServerError, Spinner } from "../../shared";
 
 export function ShopPage() {
   // hook useState per aggiornare componente ogni volta che lo stato cambia
   const [products, setProducts] = useState<Product[]>([]);
   const [pending, setPending] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   // hook useEffect con [] viene eseguito solo una volta quando viene instanziato il componente
   useEffect(() => {
@@ -16,10 +18,9 @@ export function ShopPage() {
   function loadData() {
     pb.collection("products")
       .getList<Product>()
-      .then((res) => {
-        setProducts(res.items);
-        setPending(false);
-      });
+      .then((res) => setProducts(res.items))
+      .catch(() => setError(true))
+      .finally(() => setPending(false));
   }
 
   function addToCart(p: Partial<Product>) {
@@ -31,7 +32,8 @@ export function ShopPage() {
       <h1 className="title">SHOP</h1>
 
       {/*visualizzo il loading solo quando pending è true*/}
-      {pending && <div>Loading...</div>}
+      {pending && <Spinner />}
+      {error && <ServerError />}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
         {products.map((p) => {
